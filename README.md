55800ebff1f6b9b85ccf228c51edbec228bf47d9
Fernando Mejia <femeji@microsoft.com>
2018-03-01 06:31:57 -0300

    update a solo bot

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# luisBindingActionsBotOnly
+# luisBindingActions
---
+++ all.js
@@ -0,0 +1,6 @@
+// export samples
+module.exports = []
+    .concat(require('./sampleActions/time'))
+    .concat(require('./sampleActions/hotels'))
+    .concat(require('./sampleActions/weather'))
+    .concat(require('./sampleActions/airports'));
